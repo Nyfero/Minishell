3e4e717d52191f1793f9a8dd0810088d763a43f8
Gaetan SAP <gsap@e2r5p13.clusters.42paris.fr>
2022-02-02 11:48:54 +0100

    test pull a 2

--- minishell/srcs/ft_env_func.c
+++ minishell/srcs/ft_env_func.c
@@ -6,7 +6,7 @@
 /*   By: gsap <gsap@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 11:30:08 by gsap              #+#    #+#             */
-/*   Updated: 2022/02/02 11:31:55 by gsap             ###   ########.fr       */
+/*   Updated: 2022/02/02 11:46:14 by gsap             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 void	ft_getenv(t_env **env, char **envp)
 {
+	int	i;
+	int	len_envp;
+
 	if (!env)
 	{
 		printf("NO ENV\n");
 		return (NULL);
 	}
+	i = 0;
 
+	*env = create_env_list(envp);
 }
